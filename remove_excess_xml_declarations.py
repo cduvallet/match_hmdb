bfbe23724350adc8aae5b50c174c6505db46be40
duvallet <duvallet@mit.edu>
2016-05-02 15:49:55 -0400

    Add command line interface to remove_declarations code

--- remove_excess_xml_declarations.py
+++ remove_excess_xml_declarations.py
@@ -1,15 +1,22 @@
 import re
-from lxml import etree
-#Path to hmdb xml file
-hmdb_all_metabolites = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/hmdb_metabolites/hmdb_metabolites.xml'
-#Path to your output file
-output_file = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/hmdb_metabolites/removed_xml_hmdb_metabolites.xml'
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', help='input "xml file", concatenated from many individual xml files', default='', required=True)
+    parser.add_argument('-o', help='output xml file, with excess declarations removed', default='', required=True)
+    args = parser.parse_args()
+    inputxml = args.i
+    outputxml = args.o
+else:
+    #Path to hmdb xml file
+    inputxml = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/hmdb_metabolites/hmdb_metabolites.xml'
+    #Path to your output file
+    outputxml = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/hmdb_metabolites/removed_xml_hmdb_metabolites.xml'
 
 xml_top_tag = 'database'
 
-#print etree.XML('<metabolite>', xml_declaration=True)
-
-with open(hmdb_all_metabolites, 'r+') as f, open(output_file, 'w') as output:
+with open(inputxml, 'r+') as f, open(outputxml, 'w') as output:
 	#Add a new root to the xml - you can only have one root
 	for line_num, line in enumerate(f):
 		#If the line contains an xml header, don't write it to
